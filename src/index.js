ddc3fc09e45cce51d5ac2dfc1bdab14217cc552a
Keshav singh <61233273+keshavsingh123@users.noreply.github.com>
2020-08-06 02:02:04 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//if you run locally
+//npm i @material-ui/core
+//npm i @material-ui/icons(you can search and see command)
